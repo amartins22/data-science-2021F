3a77f8228052c6265d4fcf97d6b24fe2b6a1b768
amartins22 <83664907+amartins22@users.noreply.github.com>
2021-09-20 13:52:51 -0400

    some more work on project

--- unit-1/hangman/hangman.py
+++ unit-1/hangman/hangman.py
@@ -1,6 +1,27 @@
-wordList = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "brown", "gray", "white"] 
-
-
-def chooseWord():
-    numPossibles = len(wordList)
-    word = 
+import random
+def playGame():  
+    #variables
+    wordList = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "brown", "gray", "white"] 
+    word = random.choice(wordList)
+    wordLength = len(word)
+    numGuesses = 0
+    titleMessage = """Welcome to Hangman. 
+The secret word has """ + str(wordLength) + """ letters and the theme is colors."""
+   
+    
+    def createDisplay():
+        displayWord = []
+        
+        for x in word:
+            displayWord.append("*")
+        
+        print(displayWord)
+    
+    
+    
+    print(titleMessage)
+    print(word)    
+    createDisplay()
+    
+    
+playGame()
