94d78a1b31d967045ccbabc354729365eb1fa716
amartins22 <83664907+amartins22@users.noreply.github.com>
2021-09-21 13:36:52 -0400

    working on hangman!

--- unit-1/hangman/hangman.py
+++ unit-1/hangman/hangman.py
@@ -1,27 +1,54 @@
 import random
+
+#display the secret word
+def createDisplay(displayWord, word):
+    for x in word:
+        displayWord.append("*")
+    return displayWord
+    
+#guess letter
+def guess(numGuesses):
+    g = input("Guess Letter: ")
+    print("You guessed " + g)
+    numGuesses = numGuesses - 1
+    return g
+    
+        
+#check letter
+def check(word,letter,displayWord):
+    for i in range(len(word)):
+        x = word[i]
+        if x == letter :
+            displayWord[i] = x
+            print("Yes, " + letter + " is in the word!")
+    print(displayWord)    
+
 def playGame():  
     #variables
     wordList = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "brown", "gray", "white"] 
     word = random.choice(wordList)
     wordLength = len(word)
-    numGuesses = 0
+    numGuesses = 10
+    displayWord = []
+    
     titleMessage = """Welcome to Hangman. 
-The secret word has """ + str(wordLength) + """ letters and the theme is colors."""
+The secret word has """ + str(wordLength) + """ letters and the theme is colors.
+"""
    
-    
-    def createDisplay():
-        displayWord = []
-        
-        for x in word:
-            displayWord.append("*")
-        
-        print(displayWord)
-    
-    
-    
+
     print(titleMessage)
     print(word)    
-    createDisplay()
+    createDisplay(displayWord, word)
+    print(displayWord)
+    
+    if "*" in displayWord:
+       if numGuesses > 0 :    
+           letter = guess(numGuesses)
+           check(word,letter,displayWord)
+       else :
+           print("You ran out of turns, you lose!")
+    else: 
+        print("You've guessed the word!")
     
     
 playGame()
